0acd5b317040d40e48ebe0b046f77000a7324d6c
waihouC <changwh5@gmail.com>
2022-12-22 11:19:13 +0800

    added routes and views

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const hbs = require("hbs");
+const wax = require("wax-on");
 const { sequelize } = require('./sequelize/models')
 
 let app = express();
+app.set("view engine", "hbs");
+wax.on(hbs.handlebars);
+wax.setLayoutPath("./views/layouts");
 
 async function connectToDB() {
     try {
--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const countryList = require('country-list');
+const countryInfo = countryList.getCodeList();
 
 router.get('/', async(req, res) => {
-    res.send("<h1>Welcome!</h1>");
+    res.render('main', {
+        'countryInfo': countryInfo
+    });
+});
+
+router.post('/', async(req, res) => {
+    console.log("inside test");
+    console.log(req.body.country);
+    //res.render('main');
 });
 
 module.exports = router;
